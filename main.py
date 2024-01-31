bded6123caca8daffa59449b9a5a79b24ab48d5e
kimandre1 <kimandre.tangnes@gmail.com>
2024-01-31 10:35:21 +0100

    debug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import yaml
+import yaml, tkinter
 
 with open("config.yml", "r") as yaml_file:
     config = yaml.safe_load(yaml_file)
@@ -9,4 +9,4 @@ DB_HOST = config["database"]["host"]
 DB_PORT = config["database"]["port"]
 DB_NAME = config["database"]["name"]
 
-print(DB_USER)
+print(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, sep="\n")
