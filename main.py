d18f40bfff613e5185ed1fdd0eeec266d2809476
kimandre1 <kimandre.tangnes@gmail.com>
2024-01-31 10:46:11 +0100

    Added GUI launch from main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import yaml, tkinter
+import yaml, tkinter as tk
+from gui import SimpleGUI
 
 with open("config.yml", "r") as yaml_file:
     config = yaml.safe_load(yaml_file)
@@ -10,3 +11,8 @@ DB_PORT = config["database"]["port"]
 DB_NAME = config["database"]["name"]
 
 print(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, sep="\n")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SimpleGUI(root)
+    root.mainloop()
