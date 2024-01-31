45fd83acab7ec895ea5217febf4ee388f3501a75
kimandre1 <kimandre.tangnes@gmail.com>
2024-01-31 09:34:03 +0100

    Added configuration file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello, world!")
+import yaml
+
+with open("config.yml", "r") as yaml_file:
+    config = yaml.safe_load(yaml_file)
+
+DB_USER = config["database"]["user"]
+DB_PASSWORD = config["database"]["password"]
+DB_HOST = config["database"]["host"]
+DB_PORT = config["database"]["port"]
+DB_NAME = config["database"]["name"]
+
+print(DB_USER)
